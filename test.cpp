7e94a14c2835e659e5736c17e1e60114a4831f10
NT Wallace <ntw1313@gmail.com>
2017-12-02 17:25:28 -0500

    Added test.cpp
    Needs fix in countTrigrams(), atoi not getting int

---
+++ test.cpp
@@ -0,0 +1,55 @@
+//Final Project Milestone 1
+
+#include <string>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+    
+//chars to look for are lowercase a-z, space (' '), and newline ('\n'). 
+std::vector<int> countTrigrams(std::string text){//takes a string for milestone 1
+
+    double length = pow(27.0, 3.0);
+    std::vector<int> frequencies(length);
+    
+    for (int i = 0; i < (int) text.length() - 2; i+=1) {
+        std::string trigram = text.substr(i,3);
+        std::cout << trigram << std::endl;
+        std::string a = trigram.substr(0,1);
+        std::string b = trigram.substr(1,1);
+        std::string c = trigram.substr(2,1);
+        
+        const char* c_string = a.c_str(); 
+        int indexa = atoi(c_string);
+        c_string = b.c_str();
+        int indexb = atoi(c_string);
+        c_string = c.c_str();
+        int indexc = atoi(c_string);
+        
+        int index = indexa * 729 + indexb * 27 + indexc;
+        std::cout << index << std::endl;
+        frequencies[index] = frequencies[index] + 1;
+        
+    }
+    
+    return frequencies;
+    
+}
+
+void printFreq(std::vector<int> frequencies){
+    for(int i = 0; i < frequencies.size(); i++){
+        std::cout << frequencies[i];
+        std::cout << " ";
+    }
+    
+}
+
+int main(){
+    std::string testInput = "this is a test";
+    std::vector<int> frequencies = countTrigrams(testInput);
+    printFreq(frequencies);
+    
+}
+
